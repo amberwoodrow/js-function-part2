bc629ac16d037501a4a66bc292e5f7bb36de7016
amberwoodrow <amberwoodrow@gmail.com>
2015-07-16 19:29:23 -0600

    Mission complete

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # JavaScript Functions - part 2
 
 1. Create a blank, valid HTML5 document.
-1. Create a JavaScript file
-1. Solve each of the given problems, creating a function that returns the value. Then output the result of the function to the JavaScript console.
+2. Create a JavaScript file
+3. Solve each of the given problems, creating a function that returns the value. Then output the result of the function to the JavaScript console.
 
 ### Example
 
@@ -17,7 +17,7 @@ console.log(test("ing"))
 ## Problems
 
 1. Define a function called `oldestPerson` that takes an object with names as keys and ages as values (e.g., {'Chuck': 22, 'Brian': 46}), and then returns the name of the oldest living person.
-1. Define a function called `longestWord` that takes a string and returns the longest word in the string.
-1. Refactor the `longestWord` function so that it ignores punctuation.
-1. Define a function called `factorial` that takes a random number as an argument and then returns the factorial of that given number.
-1. **Bonus**: Write a function called `palindrome` that takes a string as an argument and returns the string in reversed order. Return true if the string is the same as the reversed string, otherwise return false.
+2. Define a function called `longestWord` that takes a string and returns the longest word in the string.
+3. Refactor the `longestWord` function so that it ignores punctuation.
+4. Define a function called `factorial` that takes a random number as an argument and then returns the factorial of that given number.
+5. **Bonus**: Write a function called `palindrome` that takes a string as an argument and returns the string in reversed order. Return true if the string is the same as the reversed string, otherwise return false.
---
+++ main.js
@@ -0,0 +1,77 @@
+// numbers 1 - 3 group work with Yusef Marra.
+
+// 1.
+console.log("---OldestPerson---");
+var people = {
+  'Betty': 20,
+  'Brian': 42,
+  'Noah': 7895,
+  'Bobby': 2
+};
+
+function oldestPerson (list){
+  var oldestAge = 0; // stores the oldest age
+  var oldestName = ""; // stores the oldest person's name
+  for(i in list) {
+    if (list[i] > oldestAge) { // calculates the oldest age
+      oldestAge = list[i]; // pushes the the greatest age to be storaged
+      oldestName = i; // pushes the greatest age's name
+    }
+  }
+  return "The oldest is: " + oldestName + "! all the contestants that entered were "
+   + JSON.stringify(people); // returns by name
+}
+console.log(oldestPerson(people));
+
+
+// 2. and 3.
+console.log("---Longest word with no punctuation---")
+var string = "This is us doing a programming exercise!"
+
+function longestWord (str) {
+  var array = str.split(" "); // str to array
+  var longestWordOfString = ""; // stores the longest word of the array
+  for (i in array) { // iterates through array
+    array[i] = array[i].replace(/[^A-Za-z0-9_ ]/g, ''); // replaces punctuation with an empty string.
+    if (longestWordOfString.length < array[i].length) { // calulates longest word
+      longestWordOfString = array[i]; // pushes string to longestWordOfString
+    }
+  }
+  return "Original string: " + string + "Longest word: " + longestWordOfString;
+}
+console.log(longestWord(string));
+
+
+// Now homework to finish on our own.
+// 4.
+console.log("---Factorial---")
+function factorial (num) {
+  var factorialSum = 1;
+  if (num === 0) {
+    factorialSum = 1;
+  }else {
+    for (i=1; i<=num; i++) {
+      factorialSum = factorialSum * i;
+    }
+  }
+  return "!" + num + " is: " + factorialSum;
+}
+
+console.log(factorial(3));
+console.log(factorial(1));
+console.log(factorial(0));
+console.log(factorial(10));
+
+// 5.
+console.log("---Palindrome---");
+function palindrome(str) {
+  var strCopy = str.split("").reverse().join("");
+  if (str === strCopy){
+    return true + "! '" + str + "' spelled backwards is '" + str + "'";
+  } else {
+    return false + "! '" + str + "' spelled backwards is not '" + strCopy + "'";
+  }
+}
+
+console.log(palindrome("Hello my name is Amber"));
+console.log(palindrome("racecar"));
